2f1ecaa0a8cc862d597e3d2d938a6e252210c277
theanh <htheanh2000@gmail.com>
2024-04-16 21:35:14 +0700

    feat: add like/nope effect

--- src/components/tinder/card/index.tsx
+++ src/components/tinder/card/index.tsx
@@ -1,7 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
-import Image from 'next/image';
-
 
 interface SwipeCardProps {
   name: string;
@@ -12,37 +10,45 @@ interface SwipeCardProps {
 }
 
 const SwipeCard: React.FC<SwipeCardProps> = ({ name, age, bio, onSwipe, zIndex }) => {
-  const handleDragStart = (event: React.DragEvent) => {
-    event.preventDefault();
-  };
+  const [likeOpacity, setLikeOpacity] = useState(0);
+  const [nopeOpacity, setNopeOpacity] = useState(0);
 
   return (
     <motion.div
       drag="x"
       dragConstraints={{ left: 0, right: 0 }}
+      onDrag={(event, info) => {
+        const offset = info.offset.x;
+        // Update opacity based on drag distance dynamically
+        setLikeOpacity(Math.min(Math.max(offset / 100, 0), 1));
+        setNopeOpacity(Math.min(Math.max(-offset / 100, 0), 1));
+      }}
       onDragEnd={(event, info) => {
         const offset = info.offset.x;
         if (Math.abs(offset) > 150) {
           onSwipe();
         }
+        // Reset the opacities when drag ends
+        setLikeOpacity(0);
+        setNopeOpacity(0);
       }}
       className="bg-white max-w-sm mx-auto rounded-lg shadow-lg overflow-hidden absolute"
-      style={{ zIndex, x: 0, y: -50 * zIndex }}  // Positioning for stack effect
+      style={{ zIndex, x: 0, y: -50 * zIndex }}
       initial={{ scale: 0.95 }}
       animate={{ scale: 1 }}
       transition={{ type: "spring", stiffness: 260, damping: 20 }}
       whileDrag={{ scale: 1.1 }}
     >
-      <div className="p-4">
-        <h2 className="text-2xl">{name}, {age}</h2>
-        <p>{bio}</p>
-        <Image
-          src={'/assets/images/avatar.png'}
-          alt={name}
-          width={300}
-          height={600}
-          onDragStart={handleDragStart}
-        />
+      <div className="p-4 relative">
+        <img src={'/assets/images/avatar.png'} alt={name} width={300} height={600} draggable="false" />
+        <motion.div className="absolute top-10 left-8  border-4 rounded-md border-red" 
+          style={{ opacity: nopeOpacity }}>
+            <span className='text-red mx-4 text-3xl'>Nope</span> 
+        </motion.div>
+        <motion.div className="absolute top-10 right-8  border-4 rounded-md border-green" 
+          style={{ opacity: likeOpacity }}>
+            <span className='text-green mx-4 text-3xl'>Like</span> 
+        </motion.div>
       </div>
     </motion.div>
   );
